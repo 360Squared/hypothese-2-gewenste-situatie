9cb5bdd3f01ebdc30ae9a7c34862ba96ad2e09de
Unknown <mattle@mattle.nl>
2017-12-12 13:55:33 +0100

    Imported empty XCode project.

---
+++ Vacature Match/Vacature Match/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vacature Match
+//
+//  Created by Mattias te Wierik on 12-12-17.
+//  Copyright © 2017 360 Squared. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
